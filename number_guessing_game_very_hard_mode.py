ca39e25cfbbc14bb9205443d46181538adcc9adb
emilycharris <emily.harris.mail@gmail.com>
2016-05-17 16:59:51 -0400

    corrected guessing game reversed and added hard mode

--- number_guessing_game_very_hard_mode.py
+++ number_guessing_game_very_hard_mode.py
@@ -1,26 +1,35 @@
 import random
 
-
 player_number = int(input("Pick a number between 1 and 100: "))
 guess_list = []
 
+minimum = 1
+maximum = 100
+
+
 while len(guess_list) < 5:
-    computer_guess = random.randint(1, 100)
+    computer_guess = random.randint(minimum, maximum)
     print("I guess {}.".format(computer_guess))
     if computer_guess > player_number:
         guess_list.append(computer_guess)
         guesses_left = 5 - len(guess_list)
         if abs(player_number - computer_guess) <= 5:
             print("That number is too high, but you're close. You've taken {} turns.".format(len(guess_list)))
+            maximum = computer_guess - 1
+            minimum = computer_guess - 5
         else:
             print("That number is too high.  You've taken {} turns.".format(len(guess_list)))
+            maximum = computer_guess - 1
     elif computer_guess < player_number:
         guess_list.append(computer_guess)
         guesses_left = 5 - len(guess_list)
         if abs(player_number - computer_guess) <= 5:
             print("That number is too low, but you're close. You've taken {} turns.".format(len(guess_list)))
+            minimum = computer_guess + 1
+            maximum = computer_guess + 5
         else:
             print("That number is too low.  You've taken {} turns.".format(len(guess_list)))
+            minimum = computer_guess + 1
     else:
         guess_list.append(computer_guess)
         print("You guessed it in {} turns!".format(len(guess_list)))
